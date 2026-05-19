05c7be709feb94c690e64151bc0b14eaec4e7819
Ogunkoya Gbenga David <91924361+Gbengs1o@users.noreply.github.com>
2026-05-19 23:26:21 +0100

    Add animation annotation and flipbook tools

--- electron/preload.js
+++ electron/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld("motionKing", {
   newWindow: () => ipcRenderer.invoke("window:new"),
   openFile: () => ipcRenderer.invoke("file:open"),
   readFile: (filePath) => ipcRenderer.invoke("file:read", filePath),
-  copyPng: (dataUrl) => ipcRenderer.invoke("clipboard:copyPng", dataUrl)
+  copyPng: (dataUrl) => ipcRenderer.invoke("clipboard:copyPng", dataUrl),
+  captureRegion: (rect) => ipcRenderer.invoke("window:captureRegion", rect)
 });
